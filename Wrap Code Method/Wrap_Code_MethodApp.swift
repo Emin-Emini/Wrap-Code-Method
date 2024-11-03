edca1bf0d8a8bf0925802ada8e2452436f0ea1fc
Emin Emini <eminrmae@gmail.com>
2024-11-03 21:13:00 +0100

    Initial Commit

---
+++ Wrap Code Method/Wrap_Code_MethodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wrap_Code_MethodApp.swift
+//  Wrap Code Method
+//
+//  Created by Emin Emini on 3.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Wrap_Code_MethodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
